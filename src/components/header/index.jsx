1ffcd028c7daf0ac853398aa95826ef683c2f83c
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-11-15 17:29:02 -0300

    Projeto ucs novo - Clinica estética

---
+++ src/components/header/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './styles.css'
+
+export default props => {
+    return(
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="header">
+                    <a className="item" href="#" id="clinica">Marcação</a>
+                </div>
+
+                <div className="navbar-container">
+                    <ul>
+                        <li><a href="#/Products">Agendados</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#/Cadastro">Horários disponiveis</a></li>
+                    </ul>                    
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/marcacao/index.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import './styles.css'
+
+export default props => {
+    const [marc, setMarc] = useState("")
+    return(
+        <div className="container">
+            <label>Nome paciente:</label>
+            <input type="text" />
+
+            <label>Dia</label>
+            <input type="date" />
+
+            <label>Hora</label>
+            <input type="time" />
+        </div>
+    )
+}
